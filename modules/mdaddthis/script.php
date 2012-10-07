d3c3cb6ade5151b8665e0fadd28d45980bf207be
DELSOL Mikaël <delsol.mikael@gmail.com>
2012-10-07 16:49:30 +0200

    [UPDATE] change of behaviour
    [UPDATE] change of behaviour

---
+++ modules/mdaddthis/script.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Script d'affichage du contenu du script javascript pour la page
+ * @date 05/07/2011
+ */
+/**
+ * Contenu
+ */
+$tpl = $operatorParameters = $rootNamespace = $currentNamespace = $operatorValue = $namedParameters = array(
+																											'display_tags'=>false);
+$content = googleAnalytics::instance()->modify($tpl,'display_ga',$operatorParameters,$rootNamespace,$currentNamespace,$operatorValue,$namedParameters);
+/**
+ * Pas de pagelayout
+ */
+$Result['pagelayout'] = false;
+if(class_exists('JSMin',true))
+	$content = JSMin::minify($content);
+else
+{
+	$ezjscINI = eZINI::instance('ezjscore.ini');
+	foreach($ezjscINI->variable('eZJSCore','JavaScriptOptimizer') as $optimizer)
+		$content = call_user_func(array(
+										$optimizer,
+										'optimize'),$content,3);
+}
+$Result['content'] = $content;
+?>
